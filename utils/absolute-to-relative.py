19696ee6ef32202dbeb9c337d155dc0ef8e2598c
Yuki Koyama <yuki@koyama.xyz>
2020-04-28 14:33:17 +0900

    Convert html files

--- utils/absolute-to-relative.py
+++ utils/absolute-to-relative.py
@@ -1,11 +1,18 @@
 # cd /path/to/web2018
 # python3 ./utils/absolute-to-relative.py
 
+import re
+
 sub_pages = [
     "archive",
     "award",
     "call-for-papers",
     "call-for-participants",
+    "call-for-participants/challenge",
+    "call-for-participants/design-competition",
+    "call-for-participants/sponsorship",
+    "call-for-participants/student_volunteer",
+    "call-for-participants/web-design",
     "committee",
     "demo-poster",
     "local",
@@ -20,4 +27,6 @@ for sub_page in sub_pages:
     html = open(path).read()
     html = html.replace("=\"/", "=\"../")
     html = html.replace("=\'/wp", "=\'../wp")
+    html = html.replace("\n0\n", "")
+    html = re.sub("[a-f0-9]+(\r)*\n\<", "<", html)
     open(path, 'w').write(html)
